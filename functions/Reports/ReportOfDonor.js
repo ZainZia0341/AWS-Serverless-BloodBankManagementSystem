9ad074787f17df168c627ce1d2976fd26dae3a74
Zain Zia <Zainzia0341@gmail.com>
2024-04-22 21:02:30 +0500

    all func working

--- functions/Reports/ReportOfDonor.js
+++ functions/Reports/ReportOfDonor.js
@@ -3,7 +3,11 @@ const AWS = require("aws-sdk")
 const handler = async (event) => {
     const dynamodb = new AWS.DynamoDB.DocumentClient();
     try {
-        const results = await dynamodb.scan({ TableName: "awsserverlessbloodbankmanagementsystem-dev-DynamoDBDonorTable-DXB6PPASX8SH" }).promise()
+        const { id } = event.pathParameters
+        const results = await dynamodb.get({ 
+            TableName: "awsserverlessbloodbankmanagementsystem-dev-DynamoDBDonorTable-DXB6PPASX8SH",
+            Key: {id}
+        }).promise()
         return {
             statusCode: 200,
             body: JSON.stringify(results),
--- functions/Reports/ReportOfPatient.js
+++ functions/Reports/ReportOfPatient.js
@@ -3,7 +3,11 @@ const AWS = require("aws-sdk")
 const handler = async (event) => {
     const dynamodb = new AWS.DynamoDB.DocumentClient();
     try {
-        const results = await dynamodb.scan({ TableName: "awsserverlessbloodbankmanagementsystem-dev-DynamoDBPatientTable-1A9W56M44ANZT" }).promise()
+        const { id } = event.pathParameters
+        const results = await dynamodb.get({ 
+            TableName: "awsserverlessbloodbankmanagementsystem-dev-DynamoDBPatientTable-1A9W56M44ANZT",
+            Key: {id}
+        }).promise()
         return {
             statusCode: 200,
             body: JSON.stringify(results),
