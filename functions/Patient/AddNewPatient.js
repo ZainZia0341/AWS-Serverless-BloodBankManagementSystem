e21c6cb2eaaa90f377b0595726aa80da12eb765f
Zain Zia <Zainzia0341@gmail.com>
2024-04-25 22:08:34 +0500

    adding user data to dynamodb on postconfirmation

--- functions/Patient/AddNewPatient.js
+++ functions/Patient/AddNewPatient.js
@@ -1,44 +1,83 @@
-const { v4 } = require("uuid")
-const AWS = require("aws-sdk")
+// const { v4 } = require("uuid")
+// const AWS = require("aws-sdk")
 
-const handler = async (event) => {
-  const { Name, FatherName, Age, Gender } = JSON.parse(event.body)
-  const dynamodb = new AWS.DynamoDB.DocumentClient()
-  const id = v4()
+// const handler = async (event) => {
+//   const { Name, FatherName, Age, Gender } = JSON.parse(event.body)
+//   const dynamodb = new AWS.DynamoDB.DocumentClient()
+//   const id = v4()
 
-  const NewPatient = {
-    id,
-    Name,
-    FatherName,
-    Age,
-    Gender
-  }
+//   const NewPatient = {
+//     id,
+//     Name,
+//     FatherName,
+//     Age,
+//     Gender
+//   }
 
-  // try {
-  //   const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
-  //   const params = {
-  //       GroupName: 'Patient',
-  //       Username: 'hasnain', 
-  //       UserPoolId: 'us-east-1_SYkgZDH1r'
-  //   };
-  //   let res =await cognitoIdentityServiceProvider.adminAddUserToGroup(params).promise();
-  //   console.log('User added to group successfully.',res );
-  // } catch (err) {
-  //   console.error('Error adding user to group:', err);
-  // }
+//   // try {
+//   //   const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+//   //   const params = {
+//   //       GroupName: 'Patient',
+//   //       Username: 'hasnain', 
+//   //       UserPoolId: 'us-east-1_SYkgZDH1r'
+//   //   };
+//   //   let res =await cognitoIdentityServiceProvider.adminAddUserToGroup(params).promise();
+//   //   console.log('User added to group successfully.',res );
+//   // } catch (err) {
+//   //   console.error('Error adding user to group:', err);
+//   // }
 
-  await dynamodb.put({
-    TableName: "awsserverlessbloodbankmanagementsystem-dev-DynamoDBPatientTable-1A9W56M44ANZT",
-    Item: NewPatient
-  }).promise()
-  console.log(NewPatient)
-  return {
-    statusCode: 200,
-    body: JSON.stringify(NewPatient),
-  };
+//   await dynamodb.put({
+//     TableName: "awsserverlessbloodbankmanagementsystem-dev-DynamoDBPatientTable-1A9W56M44ANZT",
+//     Item: NewPatient
+//   }).promise()
+//   console.log(NewPatient)
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(NewPatient),
+//   };
 
-}
+// }
 
-module.exports = {
-  handler
-}
+// module.exports = {
+//   handler
+// }
+
+
+// import { Auth } from 'aws-amplify';
+// const user =  await Auth.currentAuthenticatedUser();
+// // Returns an array of groups
+// const groups = user.signInUserSession.accessToken.payload["cognito:groups"];
+// const groups = user.signInUserSession.accessToken.payload["cognito:groups"];
+// groups.includes('admin'); // true
+
+
+
+// const AWS = require('aws-sdk');
+
+// exports.handler = async (event) => {
+//     const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+
+//     const params = {
+//         UserPoolId: 'YourUserPoolId', // Replace with your Cognito User Pool ID
+//         Username: event.requestContext.authorizer.claims['cognito:username'] // Assuming you're using Cognito authorizer
+//     };
+
+//     try {
+//         const userGroups = await cognitoidentityserviceprovider.adminListGroupsForUser(params).promise();
+        
+//         // Check if the user belongs to the admin group
+//         const isAdmin = userGroups.Groups.some(group => group.GroupName === 'admin');
+        
+//         if (isAdmin) {
+//             // Allow the PUT operation to proceed
+//             return { statusCode: 200, body: JSON.stringify("User is in the admin group. Allowing PUT operation.") };
+//         } else {
+//             // Deny the PUT operation
+//             return { statusCode: 403, body: JSON.stringify("User is not authorized to perform this operation.") };
+//         }
+//     } catch (err) {
+//         console.error(err);
+//         return { statusCode: 500, body: JSON.stringify("Internal server error.") };
+//     }
+// };
--- functions/Patient/PostConfirmation.js
+++ functions/Patient/PostConfirmation.js
@@ -1,7 +1,28 @@
 const AWS = require("aws-sdk")
+const { v4 } = require("uuid")
+
+
 const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
 
 const handler = async (event, context) => {
+  // const { Name, FatherName, Age, Gender } = JSON.parse(event.body)
+  const dynamodb = new AWS.DynamoDB.DocumentClient()
+  const id = v4()
+
+  const NewPatient = {
+    id,
+    Name: event.userName,
+    FatherName: event.request.userAttributes.family_name,
+    Email: event.request.userAttributes.email
+  }
+
+  await dynamodb.put({
+    TableName: "awsserverlessbloodbankmanagementsystem-dev-DynamoDBPatientTable-1A9W56M44ANZT",
+    Item: NewPatient
+  }).promise()
+  console.log(NewPatient)
+
+
   console.log("event", JSON.stringify(event))
   console.log("userPoolId", event.userPoolId, "UserName", event.userName)
     // const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -14,7 +35,7 @@ const handler = async (event, context) => {
         let res =await cognitoIdentityServiceProvider.adminAddUserToGroup(params).promise();
         console.log('User added to group successfully.',res );
 
-        return {"checkingReturn": event};
+        return event;
       } catch (err) {
         console.error('Error adding user to group:', err);
       }
